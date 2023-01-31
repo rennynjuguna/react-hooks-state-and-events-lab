3082d99b6600dd0812e6301339742b77aa8bdf7d
Renny Muragu <118157651+rennynjuguna@users.noreply.github.com>
2023-01-31 15:52:17 +0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  function handleClick(){
+    setclassName(()=> !inCart)
+  }
+  const [inCart, setclassName]=useState(false)
   return (
-    <li className="">
+    <li className={inCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{inCart? "Remove From Cart" : "Add To Cart"}</button>
     </li>
   );
 }
